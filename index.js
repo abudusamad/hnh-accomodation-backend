d733a25896e5d137a81d8523f297bbfdb60ac698
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-08-25 10:23:49 +0000

    Testing the connection of the mongoDB connection

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
+
 import express from "express";
-import dotevn from "dotenv"
-dotevn.config();
-const app = express();
-app.listen(8800, () => {
-	console.log("connected to backend.");
-});
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+const app = express()
+dotenv.config()
+
+const connect = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO)
+		console.log("connecteyo mongoose!")
+	} catch (error) {
+		throw error;
+	}
+	
+};
+
+mongoose.connection.on("connected", () => {
+	console.log("mongoDB is connected")
+})
+
+mongoose.connection.on("disconnected", () => {
+	console.log("mongoDB is disconnected! ")
+})
+app.listen(6000, () => {
+		connect()
+		console.log("connected to backend");
+	})
+	
