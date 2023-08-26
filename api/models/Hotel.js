2f77488313c4d7c093a0fceff887f457d81a8946
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-08-26 01:42:51 +0000

    Working the hotel api for the backend

--- api/models/Hotel.js
+++ api/models/Hotel.js
@@ -2,22 +2,47 @@ import mongoose, { Schema } from "mongoose";
 const { Schema } = mongoose;
 
 const HostelSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        require:true
-    },
-    address: {
-        type: String,
-        require:true
-    }, 
-    rating: {
-        type: String,
-        require:true
-    },
+	name: {
+		type: String,
+		require: true,
+	},
+	address: {
+		type: String,
+		require: true,
+	},
+	rating: {
+		type: Number,
+		min: 0,
+		max: 5,
+	},
 
-    descriptions: {
-        type: String,
-        type: true
-    },
-    
-})
+	descriptions: {
+		type: String,
+		require: true,
+	},
+	city: {
+		type: String,
+		require: true,
+	},
+	photos: {
+		type: [String],
+		require: true,
+	},
+	rooms: {
+		type: [String],
+	},
+	location: {
+		type: String,
+		require: true,
+	},
+	price: {
+		type: Number,
+		require: true,
+	},
+	features: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+export default mongoose.model("Hostel", HostelSchema);
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,17 +1,21 @@
 import express from "express";
+import Hotel from "../models/Hotel.js";
 
-
-const router = express.Router(  )
+const router = express.Router();
 //CREATE
 
-router.post("/", (req, res) => {
-    
-})
+router.post("/", async (req, res) => {
+	const newHotel = new Hotel(req.body);
+	try {
+		const savedHotel = await newHotel.save();
+		res.status(200).json(savedHotel);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 //UPDATE
 //DELETE
 //GET
-//GET ALL           
-
+//GET ALL
 
 export default router;
- 
