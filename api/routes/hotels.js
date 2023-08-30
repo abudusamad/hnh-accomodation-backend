83037a96b04a1474437246fb999cdb703e6abd44
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-08-30 00:01:33 +0000

    finished with the api for the hostel

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -17,11 +17,46 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
 	const newHotel = new Hotel(req.body);
 	try {
-		const updateHotel = await Hotel.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
+		const updateHotel = await Hotel.findByIdAndUpdate(
+			req.params.id,
+			{ $set: req.body },
+			{ new: true }
+		);
 		res.status(200).json(updateHotel);
 	} catch (err) {
 		res.status(500).json(err);
 	}
 });
 //DELETE
-router.delete("/:id", async (req, res)
+router.delete("/:id", async (req, res) => {
+	const newHotel = new Hotel(req.body);
+	try {
+		await Hotel.findByIdAndDelete(req.params.id);
+		res.status(200).json("Hotel has been deleted");
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+//GET
+router.get("/:id", async (req, res) => {
+	const newHotel = new Hotel(req.body);
+	try {
+		const hotel = await Hotel.findById(req.params.id);
+		res.status(200).json(hotel);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+//GET ALL
+router.put("/", async (req, res) => {
+	const newHotel = new Hotel(req.body);
+	try {
+		const hotels = await Hotel.find(req.params.id);
+		res.status(200).json(hotel);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+export default router;
