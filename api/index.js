151ac8d2a79d5acb35b4cfb750a678521d8da7c0
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-08-30 00:16:30 +0000

    installing some dependency for the api

--- api/index.js
+++ api/index.js
@@ -1,40 +1,51 @@
-import dotenv from "dotenv";
 import express from "express";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
-import usersRoute from "./routes/users.js"
-import hostelsRoute from "./routes/hotels.js";
+import usersRoute from "./routes/users.js";
+import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
 const app = express();
 dotenv.config();
 
 const connect = async () => {
 	try {
 		await mongoose.connect(process.env.MONGO);
-		console.log("connecte to mongoose!");
+		console.log("Connected to mongoDB.");
 	} catch (error) {
 		throw error;
 	}
 };
 
-mongoose.connection.on("connected", () => {
-	console.log("mongoDB is connected");
+mongoose.connection.on("disconnected", () => {
+	console.log("mongoDB disconnected!");
 });
 
-mongoose.connection.on("disconnected", () => {
-	console.log("mongoDB is disconnected! ");
-});
+//middlewares
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
 
 app.use("/api/auth", authRoute);
-app.use("/api/hostels", hostelsRoute);
 app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
-
-app.get("/", (req, res) => {
-	res.send("The first websitet of ");
+app.use((err, req, res, next) => {
+	const errorStatus = err.status || 500;
+	const errorMessage = err.message || "Something went wrong!";
+	return res.status(errorStatus).json({
+		success: false,
+		status: errorStatus,
+		message: errorMessage,
+		stack: err.stack,
+	});
 });
+
 app.listen(8800, () => {
-	connect()
-	console.log("connected to backend");
+	connect();
+	console.log("Connected to backend.");
 });
