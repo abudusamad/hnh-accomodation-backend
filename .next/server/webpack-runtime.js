0494a309d4b0e10552cbcf2a64f4eebdcafe20e2
abudusamad <abudusamed@gmail.com>
2023-12-04 16:43:57 +0000

    Setting up the configuration files and folder

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("8948af9859336bb3")
+/******/ 		__webpack_require__.h = () => ("4471cae34be1cd9f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5eead6d69eddb3dc"; }
+/******/ 		__webpack_require__.h = function() { return "24286befcffb9aa9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
