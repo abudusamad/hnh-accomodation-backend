9881f85c75312688ce7963e344d31750a94b5c76
abudusamad <abudusamed@gmail.com>
2024-01-17 23:56:27 +0000

    okay

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,6 +47,11 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	(() => {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -125,7 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("4471cae34be1cd9f")
+/******/ 		__webpack_require__.h = () => ("ce5a70c5579580a0")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      Hello world
+      Hello worldsdfsad
     </div>
   )
  
